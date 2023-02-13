134033ab0701720fb93524164e71c7d9c9476ef6
unknown <butwhyilove@gmail.com>
2023-02-13 23:06:03 +0300

    1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a != b)
+{
+    if (a < b)
+        Console.Write("{0} больше {1}", b, a);
+    else
+        Console.Write("{0} больше {1}", a, b);
+}
+else
+    Console.Write("Числа равны");
---
+++ zadacha2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write("{0} максимальное", a);
+    }
+    else
+        Console.Write("{0} максимальное", c);
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write("{0} максимальное", b);
+    }
+    else
+        Console.Write("{0} максимальное", c);
+}
